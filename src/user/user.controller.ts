17df026b2e843091d5c6d87b4cc22335c0d00ac6
mhmadamrii <interceptorghost4@gmail.com>
2024-09-03 07:57:29 +0700

    .

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,6 +8,7 @@ import {
   Patch,
   Param,
   Delete,
+  NotFoundException,
 } from '@nestjs/common';
 
 @Controller()
@@ -19,8 +20,24 @@ export class UserController {
     return this.userService.getAll();
   }
 
+  @Get('/user/:id')
+  async findById(@Param('id') id: string) {
+    try {
+      return this.userService.findById(id);
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+    }
+  }
+
   @Post('/user')
   create(@Body() userData: Prisma.UserCreateInput): Promise<User> {
     return this.userService.create(userData);
+  }
+
+  @Delete('/user/:id')
+  deleteUser(@Param('id') id: string) {
+    return this.userService.deleteUser(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
 import { Prisma, User } from '@prisma/client';
 
@@ -10,7 +10,24 @@ export class UserService {
     return this.prisma.user.findMany();
   }
 
+  async findById(id: string) {
+    const user = await this.prisma.user.findUnique({ where: { id } });
+    if (!user) {
+      throw new NotFoundException({
+        status: HttpStatus.NOT_FOUND,
+        code: 'NOT_FOUND',
+        message: `User with id ${id} not found.`,
+      });
+    }
+
+    return user;
+  }
+
   async create(data: Prisma.UserCreateInput): Promise<User> {
     return this.prisma.user.create({ data });
+  }
+
+  async deleteUser(id: string) {
+    return this.prisma.user.delete({ where: { id } });
   }
 }
